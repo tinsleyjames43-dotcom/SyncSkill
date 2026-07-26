d795872e6c3506535e0a448468cab4e03814f0d9
tinsleyjames43-dotcom <tinsleyjames43@gmail.com>
2026-07-26 21:59:52 +0100

    Connect App to SyncSkill navigation

--- App.js
+++ App.js
@@ -1,34 +1,6 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import Navigation from "./navigation/Navigation";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>SyncSkill</Text>
-      <Text style={styles.subtitle}>
-        Learn. Build. Connect.
-      </Text>
-    </View>
-  );
+  return <Navigation />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#111827",
-  },
-
-  title: {
-    fontSize: 42,
-    fontWeight: "bold",
-    color: "white",
-  },
-
-  subtitle: {
-    marginTop: 10,
-    fontSize: 18,
-    color: "#9CA3AF",
-  },
-});
